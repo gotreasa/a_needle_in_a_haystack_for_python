fc790edd9589029b3a3cb5fa7dd8c424fa9f9ed9
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-02 14:56:34 +0000

    refactor: simplify the code

--- modules/needle_in_haystack.py
+++ modules/needle_in_haystack.py
@@ -1,10 +1,4 @@
 def find_needle(haystack: list[any]) -> str:
-    if haystack == ["needle"]:
-        return f"✅ found the needle at position {haystack.index('needle')}"
-    if haystack == ["hay", "needle"]:
-        return f"✅ found the needle at position {haystack.index('needle')}"
-    if haystack == [4, "junk", "hay", 3, "needle", True]:
-        return f"✅ found the needle at position {haystack.index('needle')}"
-    if haystack == ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]:
-        return f"✅ found the needle at position {haystack.index('needle')}"
-    return "❗️ error - no items in the haystack"
+    if haystack == []:
+        return "❗️ error - no items in the haystack"
+    return f"✅ found the needle at position {haystack.index('needle')}"
