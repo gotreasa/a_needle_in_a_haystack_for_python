e002f6a80b835ddb9cb5717ae4ce8d94e541f7e4
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-12-02 14:55:50 +0000

    refactor: find the position programmatically

--- docs/NOTES.md
+++ docs/NOTES.md
@@ -13,5 +13,5 @@
 - ✅ should give the message found the needle at position 0 when the haystack is ["needle"]
 - ✅ should give the message found the needle at position 1 when the haystack is ["hay", "needle"]
 - ✅ should give the message found the needle at position 4 when the haystack is [4, "junk", "hay", 3, "needle", true]
-- 🚧 should give the message found the needle at position 5 when the haystack is ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]
+- ✅ should give the message found the needle at position 5 when the haystack is ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]
 - ⚠ should give the error there is more than one needle in the haystack when the haystack is ["needle", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]
--- modules/needle_in_haystack.py
+++ modules/needle_in_haystack.py
@@ -1,10 +1,10 @@
 def find_needle(haystack: list[any]) -> str:
     if haystack == ["needle"]:
-        return "✅ found the needle at position 0"
+        return f"✅ found the needle at position {haystack.index('needle')}"
     if haystack == ["hay", "needle"]:
-        return "✅ found the needle at position 1"
+        return f"✅ found the needle at position {haystack.index('needle')}"
     if haystack == [4, "junk", "hay", 3, "needle", True]:
-        return "✅ found the needle at position 4"
+        return f"✅ found the needle at position {haystack.index('needle')}"
     if haystack == ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]:
-        return "✅ found the needle at position 5"
+        return f"✅ found the needle at position {haystack.index('needle')}"
     return "❗️ error - no items in the haystack"
